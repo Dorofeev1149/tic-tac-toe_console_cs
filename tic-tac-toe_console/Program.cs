8a2a20880c93b61e41953ee328ab790413279bbd
Andrey <dorofeevav1149@gmail.com>
2025-09-03 02:39:45 +0300

    Add files via upload

--- tic-tac-toe_console/Program.cs
+++ tic-tac-toe_console/Program.cs
@@ -1,23 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.ExceptionServices;
+using System.Xml.Serialization;
 
 public class Program
 {
-    private static int Random_Choice_Player()
+    private static int RandomChoicePlayer()
     {
         Random random_player = new Random();
         int random_choice_player = random_player.Next(0, 2);
         return random_choice_player;
     }
-    private static String[] Field_Generated()
+    private static String[] FieldGenerated()
     {
         List<String> field = new List<String>();
         string[] newField = { "-", "-", "-", "-", "-", "-", "-", "-", "-" };
         field.AddRange(newField);
         return field.ToArray();
     }
-    private static void Field_Output(String[] fields)
+    private static void FieldOutput(String[] fields)
     {
         Console.WriteLine();
         short i = 0;
@@ -35,6 +36,17 @@ public class Program
         if (choice_player == 0) { player = "cross"; }
         else if (choice_player == 1) { player = "zero"; }
         return player;
+    }
+    private static int ChoosingMode()
+    {
+        while (true)
+        {
+            Console.Write("Choose the game mode (1 - with another person, 2 - with a bot): ");
+            String? answer = Console.ReadLine();
+            if (answer == "1") { Console.Clear(); Console.WriteLine("Hello! This is a game of tic-tac-toe! | By DOROFEEV"); return 1; }
+            else if (answer == "2") { Console.Clear(); Console.WriteLine("Hello! This is a game of tic-tac-toe! | By DOROFEEV"); return 2; }
+            else { Console.Clear(); Console.WriteLine("Hello! This is a game of tic-tac-toe! | By DOROFEEV"); }
+        }
     }
     private static void MakingMove(String[] fields, String player)
     {
@@ -55,7 +67,7 @@ public class Program
                     {
                         Console.Clear();
                         Console.WriteLine("Hello! This is a game of tic-tac-toe! | By DOROFEEV");
-                        Field_Output(fields);
+                        FieldOutput(fields);
                         Console.WriteLine($"The {player} walks");
                     }
                 }
@@ -63,7 +75,7 @@ public class Program
                 {
                     Console.Clear();
                     Console.WriteLine("Hello! This is a game of tic-tac-toe! | By DOROFEEV");
-                    Field_Output(fields);
+                    FieldOutput(fields);
                     Console.WriteLine($"The {player} walks");
                 }
             }
@@ -81,16 +93,16 @@ public class Program
                     {
                         Console.Clear();
                         Console.WriteLine("Hello! This is a game of tic-tac-toe! | By DOROFEEV");
-                        Field_Output(fields);
+                        FieldOutput(fields);
                         Console.WriteLine($"The {player} walks");
                         Console.Write($"Enter the value of x: {x_coord}");
                     }
                 }
-                catch (Exception e)
+                catch (Exception)
                 {
                     Console.Clear();
                     Console.WriteLine("Hello! This is a game of tic-tac-toe! | By DOROFEEV");
-                    Field_Output(fields);
+                    FieldOutput(fields);
                     Console.WriteLine($"The {player} walks");
                     Console.Write($"Enter the value of x: {x_coord}");
                 }
@@ -107,7 +119,7 @@ public class Program
             {
                 Console.Clear();
                 Console.WriteLine("Hello! This is a game of tic-tac-toe! | By DOROFEEV");
-                Field_Output(fields);
+                FieldOutput(fields);
                 Console.WriteLine($"The {player} walks");
             }
         }
@@ -140,27 +152,229 @@ public class Program
         data.AddRange(newData);
         return data;
     }
+    private static void MakingMoveWithBot(String[] fields, String player, int random_choice)
+    {
+        while (true)
+        {
+            if (random_choice == 0)
+            {
+                String signx = "", signo = "";
+                if (player == "cross") { signo = "O"; }
+                if (player == "zero") { signo = "X"; }
+                if (player == "cross") { signx = "X"; }
+                if (player == "zero") { signx = "O"; }
+                int count = fields.Count(x => x == "-");
+
+                if (fields[0] == fields[4] && fields[4] == signx && fields[8] == "-") { fields[8] = signx; break; }
+                else if (fields[8] == fields[4] && fields[4] == signx && fields[0] == "-") { fields[0] = signx; break; }
+                else if (fields[8] == fields[0] && fields[0] == signx && fields[4] == "-") { fields[4] = signx; break; }
+                else if (fields[2] == fields[4] && fields[4] == signx && fields[6] == "-") { fields[6] = signx; break; }
+                else if (fields[6] == fields[4] && fields[4] == signx && fields[2] == "-") { fields[2] = signx; break; }
+                else if (fields[6] == fields[2] && fields[2] == signx && fields[4] == "-") { fields[4] = signx; break; }
+                else if (fields[0] == fields[3] && fields[3] == signx && fields[6] == "-") { fields[6] = signx; break; }
+                else if (fields[6] == fields[3] && fields[3] == signx && fields[0] == "-") { fields[0] = signx; break; }
+                else if (fields[0] == fields[6] && fields[6] == signx && fields[3] == "-") { fields[3] = signx; break; }
+                else if (fields[1] == fields[4] && fields[4] == signx && fields[7] == "-") { fields[7] = signx; break; }
+                else if (fields[7] == fields[4] && fields[4] == signx && fields[1] == "-") { fields[1] = signx; break; }
+                else if (fields[1] == fields[7] && fields[7] == signx && fields[4] == "-") { fields[4] = signx; break; }
+                else if (fields[2] == fields[5] && fields[5] == signx && fields[8] == "-") { fields[8] = signx; break; }
+                else if (fields[8] == fields[5] && fields[5] == signx && fields[2] == "-") { fields[2] = signx; break; }
+                else if (fields[2] == fields[8] && fields[8] == signx && fields[5] == "-") { fields[5] = signx; break; }
+                else if (fields[0] == fields[1] && fields[1] == signx && fields[2] == "-") { fields[2] = signx; break; }
+                else if (fields[2] == fields[1] && fields[1] == signx && fields[0] == "-") { fields[0] = signx; break; }
+                else if (fields[0] == fields[2] && fields[2] == signx && fields[1] == "-") { fields[1] = signx; break; }
+                else if (fields[3] == fields[4] && fields[4] == signx && fields[5] == "-") { fields[5] = signx; break; }
+                else if (fields[5] == fields[4] && fields[4] == signx && fields[3] == "-") { fields[3] = signx; break; }
+                else if (fields[3] == fields[5] && fields[5] == signx && fields[4] == "-") { fields[4] = signx; break; }
+                else if (fields[6] == fields[7] && fields[7] == signx && fields[8] == "-") { fields[8] = signx; break; }
+                else if (fields[8] == fields[7] && fields[7] == signx && fields[6] == "-") { fields[6] = signx; break; }
+                else if (fields[6] == fields[8] && fields[8] == signx && fields[7] == "-") { fields[7] = signx; break; }
+
+                if (fields[0] == fields[4] && fields[4] == signo && fields[8] == "-") { fields[8] = signx; break; }
+                else if (fields[8] == fields[4] && fields[4] == signo && fields[0] == "-") { fields[0] = signx; break; }
+                else if (fields[0] == fields[8] && fields[8] == signo && fields[4] == "-") { fields[4] = signx; break; }
+                else if (fields[2] == fields[4] && fields[4] == signo && fields[6] == "-") { fields[6] = signx; break; }
+                else if (fields[6] == fields[4] && fields[4] == signo && fields[2] == "-") { fields[2] = signx; break; }
+                else if (fields[2] == fields[6] && fields[6] == signo && fields[4] == "-") { fields[4] = signx; break; }
+                else if (fields[0] == fields[3] && fields[3] == signo && fields[6] == "-") { fields[6] = signx; break; }
+                else if (fields[6] == fields[3] && fields[3] == signo && fields[0] == "-") { fields[0] = signx; break; }
+                else if (fields[0] == fields[6] && fields[6] == signo && fields[3] == "-") { fields[3] = signx; break; }
+                else if (fields[1] == fields[4] && fields[4] == signo && fields[7] == "-") { fields[7] = signx; break; }
+                else if (fields[7] == fields[4] && fields[4] == signo && fields[1] == "-") { fields[1] = signx; break; }
+                else if (fields[1] == fields[7] && fields[7] == signo && fields[4] == "-") { fields[4] = signx; break; }
+                else if (fields[2] == fields[5] && fields[5] == signo && fields[8] == "-") { fields[8] = signx; break; }
+                else if (fields[8] == fields[5] && fields[5] == signo && fields[2] == "-") { fields[2] = signx; break; }
+                else if (fields[2] == fields[8] && fields[8] == signo && fields[5] == "-") { fields[5] = signx; break; }
+                else if (fields[0] == fields[3] && fields[3] == signo && fields[6] == "-") { fields[6] = signx; break; }
+                else if (fields[0] == fields[1] && fields[1] == signo && fields[2] == "-") { fields[2] = signx; break; }
+                else if (fields[2] == fields[1] && fields[1] == signo && fields[0] == "-") { fields[0] = signx; break; }
+                else if (fields[0] == fields[2] && fields[2] == signo && fields[1] == "-") { fields[1] = signx; break; }
+                else if (fields[3] == fields[4] && fields[4] == signo && fields[5] == "-") { fields[5] = signx; break; }
+                else if (fields[5] == fields[4] && fields[4] == signo && fields[3] == "-") { fields[3] = signx; break; }
+                else if (fields[3] == fields[5] && fields[5] == signo && fields[4] == "-") { fields[4] = signx; break; }
+                else if (fields[6] == fields[7] && fields[7] == signo && fields[8] == "-") { fields[8] = signx; break; }
+                else if (fields[8] == fields[7] && fields[8] == signo && fields[6] == "-") { fields[6] = signx; break; }
+                else if (fields[6] == fields[8] && fields[8] == signo && fields[7] == "-") { fields[7] = signx; break; }
+
+                if (count == 9) { if (fields[4] == "-") { fields[4] = signx; break; } }
+                else if (count == 7)
+                {
+                    if (fields[0] == "-" && fields[8] == signo) { fields[0] = signx; break; }
+                    if (fields[2] == "-" && fields[6] == signo) { fields[2] = signx; break; }
+                    if (fields[6] == "-" && fields[2] == signo) { fields[6] = signx; break; }
+                    if (fields[8] == "-" && fields[0] == signo) { fields[8] = signx; break; }
+                    else
+                    {
+                        if (fields[0] == "-" && fields[1] == signo) { fields[0] = signx; break; }
+                        else if (fields[2] == "-" && fields[1] == signo) { fields[2] = signx; break; }
+                        else if (fields[0] == "-" && fields[3] == signo) { fields[0] = signx; break; }
+                        else if (fields[6] == "-" && fields[3] == signo) { fields[6] = signx; break; }
+                        else if (fields[2] == "-" && fields[5] == signo) { fields[2] = signx; break; }
+                        else if (fields[8] == "-" && fields[5] == signo) { fields[8] = signx; break; }
+                        else if (fields[6] == "-" && fields[7] == signo) { fields[6] = signx; break; }
+                        else if (fields[8] == "-" && fields[7] == signo) { fields[8] = signx; break; }
+                    }
+                }
+                else if (count == 5)
+                {
+                    if (fields[0] == fields[4] && fields[1] == signo && fields[6] == "-") { fields[6] = signx; break; }
+                    else if (fields[2] == fields[4] && fields[1] == signo && fields[8] == "-") { fields[8] = signx; break; }
+                    else if (fields[0] == fields[4] && fields[3] == signo && fields[2] == "-") { fields[2] = signx; break; }
+                    else if (fields[6] == fields[4] && fields[3] == signo && fields[8] == "-") { fields[8] = signx; break; }
+                    else if (fields[2] == fields[4] && fields[5] == signo && fields[0] == "-") { fields[0] = signx; break; }
+                    else if (fields[8] == fields[4] && fields[5] == signo && fields[6] == "-") { fields[6] = signx; break; }
+                    else if (fields[6] == fields[4] && fields[7] == signo && fields[0] == "-") { fields[0] = signx; break; }
+                    else if (fields[8] == fields[4] && fields[7] == signo && fields[2] == "-") { fields[2] = signx; break; }
+                }
+                else
+                {
+                    Random rand = new Random();
+                    while (true)
+                    {
+                        int random1 = rand.Next(0, 9);
+                        if (fields[random1] == "-") { fields[random1] = signx; break; }
+                    }
+                    break;
+                }
+            }
+            else if (random_choice == 1)
+            {
+
+                if (fields[0] != "-" && fields[1] != "-" && fields[2] != "-" && fields[3] != "-"
+                    && fields[4] != "-" && fields[5] != "-" && fields[6] != "-" && fields[7] != "-" && fields[8] != "-")
+                { break; }
+                Console.Clear();
+                Console.WriteLine("Hello! This is a game of tic-tac-toe! | By DOROFEEV");
+                FieldOutput(fields);
+                Console.WriteLine($"The {player} walks");
+                String? x_coord = "", y_coord = "";
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("Enter the value of x: ");
+                        x_coord = Console.ReadLine();
+                        if (0 < int.Parse(x_coord) & int.Parse(x_coord) < 4)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Hello! This is a game of tic-tac-toe! | By DOROFEEV");
+                            FieldOutput(fields);
+                            Console.WriteLine($"The {player} walks");
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Hello! This is a game of tic-tac-toe! | By DOROFEEV");
+                        FieldOutput(fields);
+                        Console.WriteLine($"The {player} walks");
+                    }
+                }                                 
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("Enter the value of y: ");
+                        y_coord = Console.ReadLine();
+                        if (0 < int.Parse(y_coord) & int.Parse(y_coord) < 4)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Hello! This is a game of tic-tac-toe! | By DOROFEEV");
+                            FieldOutput(fields);
+                            Console.WriteLine($"The {player} walks");
+                            Console.Write($"Enter the value of x: {x_coord}");
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Hello! This is a game of tic-tac-toe! | By DOROFEEV");
+                        FieldOutput(fields);
+                        Console.WriteLine($"The {player} walks");
+                        Console.Write($"Enter the value of x: {x_coord}");
+                    }
+                }
+                String sign = "";
+                if (player == "cross") { sign = "X"; }
+                if (player == "zero") { sign = "O"; }
+                int coords = 0;
+                if (short.Parse(y_coord) == 1) { coords = short.Parse(x_coord) - 1; }
+                else if (short.Parse(y_coord) == 2) { coords = short.Parse(x_coord) + 2; }
+                else if (short.Parse(y_coord) == 3) { coords = short.Parse(x_coord) + 5; }
+                if (fields[coords].Equals("-")) { fields[coords] = $"{sign}"; Console.Clear(); Console.WriteLine("Hello! This is a game of tic-tac-toe! | By DOROFEEV"); break; }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Hello! This is a game of tic-tac-toe! | By DOROFEEV");
+                    FieldOutput(fields);
+                    Console.WriteLine($"The {player} walks");
+                }
+            }
+        }
+    }
     private static void Main(String[] args)
     {
         while (true)
         {
             Console.WriteLine("Hello! This is a game of tic-tac-toe! | By DOROFEEV");
-            int choice_player = Random_Choice_Player(); // Selection of a random player // 0 - cross; 1 - zero 
-            String[] fields = Field_Generated(); // Generation of the game board
+            Random random = new Random();
+            int random_choice = 0; //random.Next(0, 2);
+            int choice_player = RandomChoicePlayer(); // Selection of a random player // 0 - cross; 1 - zero 
+            String[] fields = FieldGenerated(); // Generation of the game board
+            int mode = ChoosingMode(); // Choosing a mode ( 1 - with another person, 2 - with a bot )
             while (true)
             {
-                Field_Output(fields); // Output of the game field
+                FieldOutput(fields); // Output of the game field
                 String player = InversionPlayer(choice_player); // Inversion of the player in the title
-                Console.WriteLine($"The {player} walks");
-                MakingMove(fields, player); // Making a move
+
+                if (mode == 1)
+                {
+                    Console.WriteLine($"The {player} walks");
+                    MakingMove(fields, player); // Making a move
+                }
+                else if (mode == 2)
+                {
+                    MakingMoveWithBot(fields, player, random_choice);
+                    if (random_choice == 0) { random_choice = 1; }
+                    else if (random_choice == 1) { random_choice = 0; }
+                }
+
                 Console.Clear();
                 List<String> check = CheckWin(fields, choice_player); // Check the winnings
-                if (check[0] == "True") { Console.WriteLine($"Won the {player}"); break; }
-                if (check[1] == "True") { Console.WriteLine($"Draw!"); break; }
+                if (check[0] == "True") { FieldOutput(fields); Console.WriteLine($"Won the {player}"); break; }
+                if (check[1] == "True") { FieldOutput(fields); Console.WriteLine($"Draw!"); break; }
                 choice_player = ChangePlayer(choice_player); // Change player
             }
+            Console.ReadKey(true);
+            Console.Clear();
         }
     }
 }
-
-    
